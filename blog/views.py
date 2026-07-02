2ad93852b0a961b3d3f201229b8d5b422ad08121
kolbasa25 <63467154+kolbasa25@users.noreply.github.com>
2026-07-02 21:26:00 +1000

    show popular posts on main page.

--- blog/views.py
+++ blog/views.py
@@ -29,8 +29,9 @@ def serialize_tag(tag):
 
 
 def index(request):
-
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(
+        likes_count=models.Count('likes')
+    ).order_by('-likes_count')[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -80,7 +81,9 @@ def post_detail(request, slug):
     popular_tags = sorted(all_tags, key=get_related_posts_count)
     most_popular_tags = popular_tags[-5:]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(
+        likes_count=models.Count('likes')
+    ).order_by('-likes_count')[:5]
 
     context = {
         'post': serialized_post,
@@ -99,7 +102,9 @@ def tag_filter(request, tag_title):
     popular_tags = sorted(all_tags, key=get_related_posts_count)
     most_popular_tags = popular_tags[-5:]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(
+        likes_count=models.Count('likes')
+    ).order_by('-likes_count')[:5]
 
     related_posts = tag.posts.all()[:20]
 
